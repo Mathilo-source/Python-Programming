0c08b04b745ef7f6263dc35677743f73202bc5be
Debra Mathilo <debramutheu05@gmail.com>
2026-02-01 18:27:13 +0300

    Slicing in Python

--- Slicing.py
+++ Slicing.py
@@ -6,7 +6,7 @@ print(name[2:5])#prints from the 2nd character to the 4th one,,,5th not included
 print(name[2:])#prints from 2nd character to the last  one
 print(name * 2)#prints the name 2 times
 print(name+"Library")#concatenation
-
+print(name[::2])#slicing with steps
 #outputs
 #Meru University
 #M
@@ -14,3 +14,4 @@ print(name+"Library")#concatenation
 #ru University
 #Meru UniversityMeru University
 #Meru UniversityLibrary
+#Mr nvriy
