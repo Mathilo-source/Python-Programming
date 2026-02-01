9c9c371836ffba28839d535a10236504a2e17417
Debra Mathilo <debramutheu05@gmail.com>
2026-02-01 18:00:31 +0300

    Slicing in Python
    Added output comments for string slicing examples.

---
+++ Slicing.py
@@ -0,0 +1,16 @@
+#SLICING
+name="Meru University"
+print(name)#prints the whole name
+print(name[0])#prints the character on the 0 index
+print(name[2:5])#prints from the 2nd character to the 4th one,,,5th not included
+print(name[2:])#prints from 2nd character to the last  one
+print(name * 2)#prints the name 2 times
+print(name+"Library")#concatenation
+
+#outputs
+#Meru University
+#M
+#ru 
+#ru University
+#Meru UniversityMeru University
+#Meru UniversityLibrary
