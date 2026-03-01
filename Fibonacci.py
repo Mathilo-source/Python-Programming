db5ae51fd71ee29bd651b83f61b88d830c699535
Debra Mathilo <debramutheu05@gmail.com>
2026-03-02 01:59:05 +0300

    Fibonacci

---
+++ Fibonacci.py
@@ -0,0 +1,12 @@
+def fib(n):  # return Fibonacci series up to n
+    result = []
+    a, b = 0, 1
+    while b < n:
+        result.append(b)
+        a, b = b, a + b
+    return result
+
+
+if __name__ == "__main__":#prevents the print statement from running when the module is imported somewhere else
+    f = fib(100)
+    print(f)
